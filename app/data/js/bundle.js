776bb89f3df3a2587afc2137fe16c7957e98b9eb
Ryan Devlin <rdevlin@purdue.edu>
2018-05-11 23:12:26 -0400

    test 34

--- app/data/js/bundle.js
+++ app/data/js/bundle.js
@@ -293,13 +293,11 @@ var PiStats = function(){
         console.log(" -- TESTING FS! -- ");
         console.log(__filename);
         console.log(__dirname);
-        fs.mkdir('/temp', function() {
-                 fs.writeFile('/temp/hello-world.txt', 'Hello world!\n', function() {
-                              fs.readFile('/temp/hello-world.txt', 'utf-8', function(err, data) {
-                                          console.log(data);
-                                          });
-                              });
-                 });
+        fs.writeFile('hello-world.txt', 'Hello world!\n', function() {
+                     fs.readFile('hello-world.txt', 'utf-8', function(err, data) {
+                                 console.log(data);
+                     });
+        });
 
     };
     
